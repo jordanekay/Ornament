bb41324b47d2ccb598f857df47873861a0559868
Jordan Kay <jordanekay@twitter.com>
2013-12-24 18:16:07 -0600

    Clean up appearance setup

--- Ornament/Ornament/Views/ORNAppearance.m
+++ Ornament/Ornament/Views/ORNAppearance.m
@@ -10,11 +10,6 @@
 #import "UIFont+ORNSystem.h"
 #import "ORNSwitch.h"
 
-#define TITLE_FONT [UIFont orn_boldSystemFontOfSize:12.0f]
-#define TITLE_TEXT_COLOR [UIColor whiteColor]
-#define TITLE_TEXT_SHADOW_COLOR [UIColor colorWithWhite:0.0f alpha:0.6f]
-#define TITLE_TEXT_SHADOW_OFFSET UIOffsetMake(0.0f, -1.0f)
-#define TITLE_ADJUSTMENT -5.0f
 #define SWITCH_THUMB_INSET 3.0f
 
 @implementation ORNAppearance
