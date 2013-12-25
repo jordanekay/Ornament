71d2055843900e8a344d1e1b1eaa01e93cb95274
Jordan Kay <jordanekay@twitter.com>
2013-12-24 18:48:52 -0600

    Add EXTScope

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -24,3 +24,15 @@
 #define COCOAPODS_VERSION_MINOR_TTSwitch 0
 #define COCOAPODS_VERSION_PATCH_TTSwitch 5
 
+// libextobjc/EXTScope
+#define COCOAPODS_POD_AVAILABLE_libextobjc_EXTScope
+#define COCOAPODS_VERSION_MAJOR_libextobjc_EXTScope 0
+#define COCOAPODS_VERSION_MINOR_libextobjc_EXTScope 3
+#define COCOAPODS_VERSION_PATCH_libextobjc_EXTScope 0
+
+// libextobjc/RuntimeExtensions
+#define COCOAPODS_POD_AVAILABLE_libextobjc_RuntimeExtensions
+#define COCOAPODS_VERSION_MAJOR_libextobjc_RuntimeExtensions 0
+#define COCOAPODS_VERSION_MINOR_libextobjc_RuntimeExtensions 3
+#define COCOAPODS_VERSION_PATCH_libextobjc_RuntimeExtensions 0
+
