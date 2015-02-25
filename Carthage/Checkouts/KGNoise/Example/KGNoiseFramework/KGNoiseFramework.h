520ea8f433d6c9f61240f650186e9deec2864ef0
Jordan Kay <jordanekay@twitter.com>
2015-02-24 17:58:20 -0800

    Add initial Carthage support

---
+++ Carthage/Checkouts/KGNoise/Example/KGNoiseFramework/KGNoiseFramework.h
@@ -0,0 +1,19 @@
+//
+//  KGNoiseFramework.h
+//  KGNoiseFramework
+//
+//  Created by Jordan Kay on 2/24/15.
+//  Copyright (c) 2015 David Keegan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KGNoiseFramework.
+FOUNDATION_EXPORT double KGNoiseFrameworkVersionNumber;
+
+//! Project version string for KGNoiseFramework.
+FOUNDATION_EXPORT const unsigned char KGNoiseFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KGNoiseFramework/PublicHeader.h>
+
+
